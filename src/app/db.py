5cb264a3e10e3d3247a8b367912560f75ef05762
Ruben Jimenez Andreu <rubenjmnz93@gmail.com>
2023-11-25 23:17:00 +0100

    Updated postgres connection. Created notes table.

--- src/app/db.py
+++ src/app/db.py
@@ -1,11 +1,32 @@
 import os
 
 from databases import Database
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import (
+    Column,
+    Integer,
+    DateTime,
+    String,
+    Table,
+    create_engine,
+    MetaData
+)
+from sqlalchemy.sql import func
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
 
-engine = create_engine(DATABASE_URL)
+database_url = f"postgresql://{DB_USER}:{DB_PASS}@db/{DB_NAME}"
+
+engine = create_engine(database_url)
 metadata = MetaData()
+notes = Table(
+    "notes",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String(50)),
+    Column("description", String(50)),
+    Column("created_date", DateTime, default=func.now(), nullable=False)
+)
 
-database = Database(DATABASE_URL)
+database = Database(database_url)
--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from app.api import ping
+from app.db import engine, database, metadata
 
-app = FastAPI()
+metadata.create_all(engine)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+    await database.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(ping.router)
