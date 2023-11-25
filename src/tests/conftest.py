aa8341a72610cdcd83a622d04681c8435e6ee0c0
Ruben Jimenez Andreu <rubenjmnz93@gmail.com>
2023-11-25 20:17:24 +0100

    Configured docker, created project structure, added ping app and test, add github actions.

---
+++ src/tests/conftest.py
@@ -0,0 +1,10 @@
+from pytest import fixture
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+@fixture(scope="module")
+def test_app():
+    client = TestClient(app)
+    yield client
